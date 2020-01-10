eb7f7e0159d58fe3d6203de0e72f0a4804110496
Antoine Coulon <ancoulon@student.s19.be>
2020-01-10 07:19:22 +0100

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/10 07:12:08 by ancoulon          #+#    #+#             */
+/*   Updated: 2020/01/10 07:15:12 by ancoulon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
